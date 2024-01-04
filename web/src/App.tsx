20f68ad77dbe777f424dbfe2255c976ae474cff9
Нурсиль <n.sultanoff.2002s@gmail.com>
2024-01-04 18:03:51 +0300

    helloworld
    * Envoy: add config-yml
    
    * Proto: start protofile
    
    * Server gRPC: start helloworld service
    
    * Web-gRPC: start helloworld service

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GrpcComponent from './components/HelloName';
+
+function App() {
+  return (
+    <div className="App">
+      <GrpcComponent />
+    </div>
+  );
+}
+
+export default App;
