6dca810e7ff77e4b8d5fde16e67b2f7d727fce26
Нурсиль <n.sultanoff.2002s@gmail.com>
2024-01-18 22:54:50 +0300

    server: streaming audio service

---
+++ stream/main.py
@@ -0,0 +1,29 @@
+import grpc
+from concurrent import futures
+from generated import musicstream_pb2, musicstream_pb2_grpc
+from time import sleep
+
+
+
+class MusicStreamingServicer(musicstream_pb2_grpc.MusicStreamingServicer):
+    def StreamMusic(self, request, context):
+        with open('aws/chipi-chapa.mp3', 'rb') as audio_file:
+            chunk_size = 4096
+            while True:
+                chunk = audio_file.read(chunk_size)
+                if not chunk:
+                    break
+                yield musicstream_pb2.StreamResponse(audioChunk=chunk)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    musicstream_pb2_grpc.add_MusicStreamingServicer_to_server(MusicStreamingServicer(), server)
+    server.add_insecure_port('[::]:9090')
+    print("Server started. Listening on port 9090...")
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
+
