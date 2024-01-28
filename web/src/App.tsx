eef2a2a43be758852cc2384625fac92340f54d64
Нурсиль <n.sultanoff.2002s@gmail.com>
2024-01-28 21:42:45 +0300

    Dev (#3)
    * proxy: envoy.yaml change
    
    * protos: del hello proto
    
    * deleted GRPC testing service (hello-world)
    
    * client: del hello-world components
    
    * remove venv

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-
+<<<<<<<<< Temporary merge branch 1
+import GrpcComponent from './components/HelloName';
+=========
 import MusicPlayer from './components/Player';
-
-
+>>>>>>>>> Temporary merge branch 2
 
 function App() {
   return (
     <div className="App">
-
+<<<<<<<<< Temporary merge branch 1
+      <GrpcComponent />
+=========
       <MusicPlayer />
-
+>>>>>>>>> Temporary merge branch 2
     </div>
   );
 }
